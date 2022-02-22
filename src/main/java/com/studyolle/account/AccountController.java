abd3cd642dc0de0615edf73d328b6092dabc57ea
origin <0219alice@gmail.com>
2022-02-23 01:10:20 +0900

    SignUp Process Refactoring

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -19,8 +19,7 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder){
@@ -39,34 +38,11 @@ public class AccountController {
             return "account/sign-up";
         }
 
-        signUpFormValidator.validate(signUpForm, errors);
-        if(errors.hasErrors()){
-            return "account/sign-up";
-        }
-
-        // TODO 회원 가입 처리
-        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword()) // TODO Encoding
-                .studyCreatedByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .studyUpdatedByWeb(true)
-                .build();
-
-        Account newAccount = accountRepository.save(account);
-
-        newAccount.generateEmailCheckToken();
-
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("스터디올래, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token" + newAccount.getEmailCheckToken()
-                + "&email" + newAccount.getEmail());
-
-        javaMailSender.send(mailMessage);
+        accountService.processNewAccount(signUpForm);
         return "redirect:/";
     }
+
+
 
 }
 
