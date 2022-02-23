6eb065ef6e3006bbe005b9c9253ff1148d4a39f9
origin <0219alice@gmail.com>
2022-02-24 01:06:05 +0900

    edit mail text

--- src/main/java/com/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -42,8 +42,8 @@ public class AccountService {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(newAccount.getEmail());
         mailMessage.setSubject("스터디올래, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token" + newAccount.getEmailCheckToken()
-                + "&email" + newAccount.getEmail());
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken()
+                + "&email=" + newAccount.getEmail());
 
         javaMailSender.send(mailMessage);
     }
