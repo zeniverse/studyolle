694f323a7391b242395e2695bad7486b96222621
origin <0219alice@gmail.com>
2022-02-23 17:18:42 +0900

    apply PasswordEncoder

--- src/main/java/com/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -4,6 +4,7 @@ import com.studyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,7 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
     private final JavaMailSender javaMailSender;
+    private final PasswordEncoder passwordEncoder;
 
 
     public void processNewAccount(SignUpForm signUpForm) {
@@ -25,7 +27,7 @@ public class AccountService {
         Account account = Account.builder()
                 .email(signUpForm.getEmail())
                 .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword()) // TODO Encoding
+                .password(passwordEncoder.encode(signUpForm.getPassword())) // TODO Encoding
                 .studyCreatedByWeb(true)
                 .studyEnrollmentResultByWeb(true)
                 .studyUpdatedByWeb(true)
