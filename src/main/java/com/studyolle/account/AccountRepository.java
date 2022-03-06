9d601f6f01d06a3459696fa3e6fb29fdf52a39c6
origin <0219alice@gmail.com>
2022-03-07 00:12:57 +0900

    add findByNickname()

--- src/main/java/com/studyolle/account/AccountRepository.java
+++ src/main/java/com/studyolle/account/AccountRepository.java
@@ -10,4 +10,6 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     boolean existsByNickname(String nickname);
 
     Account findByEmail(String email);
+
+    Account findByNickname(String nickname);
 }
