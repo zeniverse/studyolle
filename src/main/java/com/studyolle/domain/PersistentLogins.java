cc2e3e516b2408373e038ab946f21aab6740a55a
origin <0219alice@gmail.com>
2022-03-08 00:13:18 +0900

    add rememberMe to SecurityConfig & generate PersistentLogins Entity

---
+++ src/main/java/com/studyolle/domain/PersistentLogins.java
@@ -0,0 +1,29 @@
+package com.studyolle.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Table(name = "persistent_logins")
+@Entity
+@Getter
+@Setter
+public class PersistentLogins {
+
+    @Id
+    @Column(length = 64)
+    private String series;
+
+    @Column(nullable = false, length = 64)
+    private String username;
+
+    @Column(nullable = false, length = 64)
+    private String token;
+
+    @Column(name = "last_used", nullable = false, length = 64)
+    private LocalDateTime lastUsed;
+
+}
+
