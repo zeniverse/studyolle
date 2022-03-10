92d403c3c002a8798cc2a3699a32177ac9b540dc
origin <0219alice@gmail.com>
2022-03-10 18:16:45 +0900

    add edit message with flashAttribute

--- src/main/java/com/studyolle/settings/SettingsController.java
+++ src/main/java/com/studyolle/settings/SettingsController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -27,13 +28,15 @@ public class SettingsController {
     }
 
     @PostMapping("/settings/profile")
-    public String updateProfile(@CurrentUser Account account, @Valid @ModelAttribute Profile profile, Errors errors, Model model){
+    public String updateProfile(@CurrentUser Account account, @Valid @ModelAttribute Profile profile, Errors errors,
+                                Model model, RedirectAttributes attributes){
         if(errors.hasErrors()){
             model.addAttribute("account");
             return "/settings/profile";
         }
 
         accountService.updateProfile(account, profile);
+        attributes.addFlashAttribute("message", "프로필을 수정했습니다.");
         return "redirect:/settings/profile";
     }
 
