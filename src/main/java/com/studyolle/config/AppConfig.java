186846792e601d4cfaa8f3266a2bd337e8d28413
origin <0219alice@gmail.com>
2022-03-14 23:08:32 +0900

    Register ModelMapper as Bean

--- src/main/java/com/studyolle/config/AppConfig.java
+++ src/main/java/com/studyolle/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.studyolle.config;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.NameTokenizers;
+import org.modelmapper.spi.NameTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -13,5 +16,15 @@ public class AppConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    public ModelMapper modelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration()
+                .setDestinationNameTokenizer(NameTokenizers.UNDERSCORE)
+                .setSourceNameTokenizer(NameTokenizers.UNDERSCORE);
+
+        return modelMapper;
+    }
 }
 
