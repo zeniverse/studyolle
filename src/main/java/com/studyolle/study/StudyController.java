de899c440f74379474297a08396b45d8da21d78a
origin <0219alice@gmail.com>
2022-03-26 01:15:28 +0900

    generate viewStudy handler

--- src/main/java/com/studyolle/study/StudyController.java
+++ src/main/java/com/studyolle/study/StudyController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -26,6 +27,7 @@ public class StudyController {
     private final StudyFormValidator studyFormValidator;
     private final StudyService studyService;
     private final ModelMapper modelMapper;
+    private final StudyRepository studyRepository;
 
     @InitBinder("studyForm")
     public void StudyFormInitBinder(WebDataBinder webDataBinder){
@@ -49,5 +51,10 @@ public class StudyController {
         return "redirect:/study/" + URLEncoder.encode(newStudy.getPath(), StandardCharsets.UTF_8);
     }
 
-
+    @GetMapping("/study/{path}")
+    public String viewStudy(@CurrentUser Account account, @PathVariable String path, Model model){
+        model.addAttribute(account);
+        model.addAttribute(studyRepository.findByPath(path));
+        return "study/view";
+    }
 }
