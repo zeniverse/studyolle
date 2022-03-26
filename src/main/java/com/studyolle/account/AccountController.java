af082fd5dbe9960b464e9ed2af4a690cbb1b2c93
origin <0219alice@gmail.com>
2022-03-27 02:05:59 +0900

    generate getAccount method

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -93,11 +93,7 @@ public class AccountController {
 
     @GetMapping("/profile/{nickname}")
     public String viewProfile(@PathVariable String nickname, Model model, @CurrentUser Account account){
-        Account byNickname = accountRepository.findByNickname(nickname);
-
-        if(nickname == null){
-            throw new IllegalArgumentException(nickname + "에 해당하는 사용자가 없습니다.");
-        }
+        Account byNickname = accountService.getAccount(nickname);
 
         model.addAttribute("account", byNickname);
         model.addAttribute("isOwner", byNickname.equals(account));
