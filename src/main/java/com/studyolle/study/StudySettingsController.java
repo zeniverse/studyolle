d0e5f336fd059fcb578b8c0e509ac81dc092de3b
origin <0219alice@gmail.com>
2022-03-28 22:33:23 +0900

    study banner setting process

--- src/main/java/com/studyolle/study/StudySettingsController.java
+++ src/main/java/com/studyolle/study/StudySettingsController.java
@@ -59,4 +59,40 @@ public class StudySettingsController {
     public String getPath(String path){
         return URLEncoder.encode(path, StandardCharsets.UTF_8);
     }
+
+    @GetMapping("/banner")
+    public String studyImageForm(@CurrentUser Account account, @PathVariable String path, Model model){
+        Study study = studyService.getStudyToUpdate(account, path);
+
+        model.addAttribute(account);
+        model.addAttribute(study);
+        return "study/settings/banner";
+    }
+
+    @PostMapping("/banner/enable")
+    public String enableStudyBanner(@CurrentUser Account account, @PathVariable String path){
+        Study study = studyService.getStudyToUpdate(account, path);
+
+        studyService.enableStudyBanner(study);
+        return "redirect:/study/" + getPath(path) + "/settings/banner";
+    }
+
+    @PostMapping("/banner/disable")
+    public String disableStudyBanner(@CurrentUser Account account, @PathVariable String path){
+        Study study = studyService.getStudyToUpdate(account, path);
+
+        studyService.disableStudyBanner(study);
+        return "redirect:/study/" + getPath(path) + "/settings/banner";
+    }
+
+    @PostMapping("/banner")
+    public String studyImageSubmit(@CurrentUser Account account, @PathVariable String path,
+                                   String image, RedirectAttributes attributes){
+        Study study = studyService.getStudyToUpdate(account, path);
+
+        studyService.updateStudyImage(study, image);
+        attributes.addFlashAttribute("message", "스터디 이미지를 수정했습니다.");
+
+        return "redirect:/study/" + getPath(path) + "/settings/banner";
+    }
 }
