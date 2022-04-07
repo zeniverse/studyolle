d8a97386cb7b5a5377a725efb76aec5dc769edbb
origin <0219alice@gmail.com>
2022-04-07 15:54:55 +0900

    ApplicationEventPublisher Process

---
+++ src/main/java/com/studyolle/modules/study/event/StudyCreatedEvent.java
@@ -0,0 +1,15 @@
+package com.studyolle.modules.study.event;
+
+import com.studyolle.modules.study.Study;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class StudyCreatedEvent{
+
+    private Study study;
+
+    public StudyCreatedEvent(Study study) {
+        this.study = study;
+    }
+}
