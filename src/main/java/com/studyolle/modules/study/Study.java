85f767469e64c1b179d3f08f72b50cfa9b5e253d
origin <0219alice@gmail.com>
2022-04-07 23:58:42 +0900

    Study Created Notification Process

--- src/main/java/com/studyolle/modules/study/Study.java
+++ src/main/java/com/studyolle/modules/study/Study.java
@@ -36,6 +36,10 @@ import java.util.Set;
 @NamedEntityGraph(name = "Study.withMembers", attributeNodes = {
         @NamedAttributeNode("members")})
 
+@NamedEntityGraph(name = "Study.withTagsAndZones", attributeNodes = {
+        @NamedAttributeNode("tags"),
+        @NamedAttributeNode("zones")})
+
 @Entity
 @Getter @Setter @EqualsAndHashCode(of = "id")
 @Builder @NoArgsConstructor @AllArgsConstructor
