f99810b997b9bf365ce74092588362ef4df4c56d
origin <0219alice@gmail.com>
2022-04-08 22:12:36 +0900

    add method for check notification

--- src/main/java/com/studyolle/modules/notification/NotificationRepository.java
+++ src/main/java/com/studyolle/modules/notification/NotificationRepository.java
@@ -1,8 +1,12 @@
 package com.studyolle.modules.notification;
 
+import com.studyolle.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    long countByAccountAndChecked(Account account, boolean checked);
+
 }
