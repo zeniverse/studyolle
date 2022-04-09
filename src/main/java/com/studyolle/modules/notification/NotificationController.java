dff574f337dcf747224d1684f1ec18d8dcefaa50
origin <0219alice@gmail.com>
2022-04-09 18:17:06 +0900

    make Notification Page Process

---
+++ src/main/java/com/studyolle/modules/notification/NotificationController.java
@@ -0,0 +1,56 @@
+package com.studyolle.modules.notification;
+
+import com.studyolle.modules.account.Account;
+import com.studyolle.modules.account.CurrentAccount;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationRepository notificationRepository;
+    private final NotificationService notificationService;
+
+    @GetMapping("/notifications")
+    public String getNotifications(@CurrentAccount Account account, Model model){
+        List<Notification> notifications
+                = notificationRepository.findByAccountAndCheckedOrderByCreatedDateTimeDesc(account, false);
+        long numberOfChecked = notificationRepository.countByAccountAndChecked(account, true);
+        putCategorizedNotifications(model, notifications, numberOfChecked, notifications.size());
+        model.addAttribute("isNew", true);
+        notificationService.markAsRead(notifications);
+
+        return "notification/list";
+    }
+
+
+    private void putCategorizedNotifications(Model model, List<Notification> notifications,
+                                             long numberOfChecked, long numberOfNotChecked) {
+
+        List<Notification> newStudyNotifications = new ArrayList<>();
+        List<Notification> eventEnrollmentNotifications = new ArrayList<>();
+        List<Notification> watchingStudyNotifications = new ArrayList<>();
+
+        for(var notification : notifications){
+            switch (notification.getNotificationType()) {
+                case STUDY_CREATED: newStudyNotifications.add(notification); break;
+                case EVENT_ENROLLMENT: eventEnrollmentNotifications.add(notification); break;
+                case STUDY_UPDATED: watchingStudyNotifications.add(notification); break;
+            }
+        }
+
+        model.addAttribute("numberOfNotChecked", numberOfNotChecked);
+        model.addAttribute("numberOfChecked", numberOfChecked);
+        model.addAttribute("notifications", notifications);
+        model.addAttribute("newStudyNotifications", newStudyNotifications);
+        model.addAttribute("eventEnrollmentNotifications", eventEnrollmentNotifications);
+        model.addAttribute("watchingStudyNotifications", watchingStudyNotifications);
+    }
+}
