8af882c8b856eada5e60cc29f7eb25b178eca786
origin <0219alice@gmail.com>
2022-04-11 15:10:54 +0900

    Search Study Process without paging

---
+++ src/main/java/com/studyolle/modules/study/StudyRepositoryExtensionImpl.java
@@ -0,0 +1,26 @@
+package com.studyolle.modules.study;
+
+import com.querydsl.jpa.JPQLQuery;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+import static com.studyolle.modules.study.QStudy.study;
+
+public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport implements StudyRepositoryExtension{
+
+    public StudyRepositoryExtensionImpl() {
+        super(Study.class);
+    }
+
+    @Override
+    public List<Study> findByKeyword(String keyword) {
+        JPQLQuery<Study> query = from(study).where(study.published.isTrue()
+                .and(study.title.containsIgnoreCase(keyword))
+                .or(study.tags.any().title.containsIgnoreCase(keyword))
+                .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword)));
+
+        return query.fetch();
+    }
+
+}
