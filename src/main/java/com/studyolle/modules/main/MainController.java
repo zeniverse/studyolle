eb40f694822ee54b3001faf56d6181c23f174ae8
origin <0219alice@gmail.com>
2022-04-12 18:50:24 +0900

    Home page without Login

--- src/main/java/com/studyolle/modules/main/MainController.java
+++ src/main/java/com/studyolle/modules/main/MainController.java
@@ -28,6 +28,8 @@ public class MainController {
             model.addAttribute(account);
         }
 
+        model.addAttribute("studyList",
+                studyRepository.findFirst9ByPublishedAndClosedOrderByPublishedDateTimeDesc(true, false));
         return "index";
     }
 
